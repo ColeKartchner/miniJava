4b15e7e7e0b72b95a40a55157131d88146be3600
ColeKartchner <cole.kartchner99@gmail.com>
2023-03-20 22:06:54 -0600

    Project 4, Typechecker class somewhat done 3/20

--- src/edu/westminstercollege/cmpt355/minijava/Typechecker.java
+++ src/edu/westminstercollege/cmpt355/minijava/Typechecker.java
@@ -10,22 +10,70 @@ public class Typechecker {
     public void typecheck(SymbolTable symbols, Node node) throws SyntaxException {
         switch (node) {
             case Block(ParserRuleContext ctx, List<Statement> statements) -> {
-
+                for (var statement : statements) {
+                    typecheck(symbols, statement);
+                }
             }
             case ExpressionStatement(ParserRuleContext ctx, Expression expression) -> {
+                typecheck(symbols, expression);
+                Type expressionType = getType(symbols, expression);
+                System.out.println("Type of " + expression + ": " + expressionType.toString());
 
             }
             case Cast(ParserRuleContext ctx, TypeNode targetType, Expression expression) -> {
+                var expressionType = getType(symbols, expression);
+                var castType = targetType.type();
+                var stringType = new ClassType("String");
+                // If a void value
+                if (expressionType.equals(VoidType.Instance)) {
+                    throw new SyntaxException(node, "A cast cannot be void");
+                } else if (!castType.equals(stringType) && !(castType instanceof PrimitiveType)) {
+                    throw new SyntaxException(node, castType + " is not a valid cast type.");
+                // Catches the types that cannot be cast to a string
+                } else if ((castType instanceof PrimitiveType) && expressionType.equals(stringType)) {
+                    throw new SyntaxException(node, castType + " cannot be cast to String");
+                // ints/doubles cannot be cast to boolean
+                } else if ((castType.equals(PrimitiveType.Int) || castType.equals(PrimitiveType.Double))
+                        && expressionType.equals(PrimitiveType.Boolean)) {
+                    throw new SyntaxException(node, castType + " cannot be cast to " + expressionType);
+                }
 
             }
             case Print(ParserRuleContext ctx, List<Expression> arguments) -> {
+                for(var expression : arguments){
+                    typecheck(symbols, expression);
+                    if(getType(symbols, expression) instanceof VoidType){
+                        throw new SyntaxException(node, "Cannot print a void value.");
+                    }
+                }
 
             }
             case VariableDeclarations(ParserRuleContext ctx, TypeNode type, List<DeclarationItem> items) -> {
+                for(var item : items) {
+                    var variable = symbols.findVariable(item.name());
+                    if(variable.isPresent()){
+                        var realVar = variable.get();
+                        realVar.setType(type.type());
+                    }
+                    if(item.children().size() != 0 ) {
+                        if(type.type().equals(PrimitiveType.Double)){
+                            if(!getType(symbols, item.initializer().get()).equals(PrimitiveType.Double)
+                                    && !getType(symbols, item.initializer().get()).equals(PrimitiveType.Int)){
+                                throw new SyntaxException(node, String.format("Cannot initialize variable: %s cannot be %s", item.name(), getType(symbols, item.initializer().get()).toString()));
+                            }
+                        }
+                        else if(!getType(symbols, item.initializer().get()).equals(type.type())){
+                            throw new SyntaxException(node, String.format("Cannot initialize variable: %s cannot be %s", item.name(), getType(symbols, item.initializer().get()).toString()));
+                        }
+                    }
+                }
 
             }
             case VariableAccess(ParserRuleContext ctx, String variableName) -> {
-
+                var variable = symbols.findVariable(variableName);
+                if (variable.isEmpty()){
+                    throw new SyntaxException(node, String.format("Variable '%s' is undefined. Please declare it before accessing it.", variableName));
+                }
             }
             case BinaryOp(ParserRuleContext ctx, Expression left, Expression right, String op) -> {
                 // Make sure the children pass their checks
@@ -41,7 +89,15 @@ public class Typechecker {
 
             }
             case Assignment(ParserRuleContext ctx, Expression target, Expression value) -> {
+                typecheck(symbols, target);
+                typecheck(symbols, value);
+                Type leftType = getType(symbols, target),
+                        rightType = getType(symbols, value);
 
+                if (!leftType.equals(rightType)
+                        && (!leftType.equals(PrimitiveType.Double) || !rightType.equals(PrimitiveType.Int))) {
+                    throw new SyntaxException(node, String.format("Error in assignment. Type: %s cannot be assigned to Type: %s", leftType, rightType));
+                }
             }
             case Negate(ParserRuleContext ctx, Expression expression) -> {
 
